5a816b60a34cff8179d27a607212b5c472d83339
Lucas José <32078930+lucasjose10@users.noreply.github.com>
2018-02-24 21:11:42 -0300

    Add files via upload

---
+++ somaf.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+ 
+int soma(int a, int b)
+{
+    int soma;
+    soma = a + b;
+    return (soma);
+}
+ 
+int main()
+{
+    int num=0;
+    int num2=0;
+    int result=0;
+ 
+    printf("Valor do 1 termo\n");
+    scanf("%d", &num);
+    printf("Valor do 2 termo\n");
+    scanf("%d", &num2);
+ 
+    result = soma(num, num2);
+ 
+    printf("Resultado: %d\n", result);
+    
+    system ("pause");
+}
+
+
