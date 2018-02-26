fe8957bcd853a418128e2bf4d0aa9e5efb98f8ba
Lucas José <32078930+lucasjose10@users.noreply.github.com>
2018-02-26 19:23:09 -0300

    Add files via upload

---
+++ fluxo.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int main(){
+
+
+    int vet[10];
+    int i=0;
+    int x=0;
+    int n=10;
+
+      printf("digite o valor do %d vetor", i+1);
+                      scanf("%d",&vet[i]);
+}
+
+                  printf("\ndigite o valor para procurar no vetor\n");
+                  scanf("%d",&x);
+
+                    if(x<n)
+
+                    for(i=0;i<10;i++){
+
+                            if(x==vet[i])
+                            {
+                                x=vet[i];
+                            }
+
+
+
+                    }
+
+
+
+                  printf("o elemento esta na posição %d", x);
+
+                  return 0;
+
+
+                    system("pause");
+                  }
+
+    for(i=0;i<10;i++){
+
+                      printf("digite o valor do %d vetor", i+1);
+                      scanf("%d",&vet[i]);
+}
+
+                  printf("\ndigite o valor para procurar no vetor\n");
+                  scanf("%d",&x);
+
+                    if(x<n)
+
+                    for(i=0;i<10;i++){
+
+                            if(x==vet[i])
+                            {
+                                x=vet[i];
+                            }
+
+
+
+                    }
+
+
+
+                  printf("o elemento esta na posição %d", x);
+
+                  return 0;
+
+
+                    system("pause");
+                  }
